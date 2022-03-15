ae627d36c69cd00ccc9be67203cdce61b2e0878b
FLAK-ZOSO <83119697+FLAK-ZOSO@users.noreply.github.com>
2022-03-15 17:08:25 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -72,44 +72,54 @@ void muoviPalla(Partita p) // Fiore
     {
         // Muovo la palla verso Sud-Est (quindi cambio p.palla.x e p.palla.y)
         p.palla.x--;
-        p.palla.y--;
+        p.palla.y++;
     }
     else if (p.palla.direzione == 3)
     {
         p.palla.x++;
-        p.palla.y--;     
+        p.palla.y++;     
+    }
+    else if (p.palla.direzione == 7)
+    {
+        p.palla.x--;
+        p.palla.y--;
     }
     else if (p.palla.direzione == 9)
     {
-        p.palla.x;
-        p.palla.y;
-    }
-    else if (p.palla == 7)
-    {
-        p.palla.x;
-        p.palla.y;
+        p.palla.x++;
+        p.palla.y--;
     }
 
-    // Mentre lo fa tiene anche conto di se vengono toccati muri, se si è nella cella di fianco a un muro e si sta andando verso il muro si deve cambiare la direzione ---> faccio girare la palla
-    if (p.palla.x == 0) 
-    {
-        
+//se incontro il muro lo faccio girare dall'altra parte
+//se arriva alla lineaprima dei bordi
+//se incontra la paletta 
+
+if (p.palla.direzione == 9)
+    p.palla.direzione = 3;
+else if (p.palla.direzione == 9)
+    p.palla.direzione = 3;
+    // Mentre lo fa tiene anche conto di se vengono toccati muri, se si è nella cella di fianco a un muro e si sta andando verso il muro si deve cambiare la direzione ---> faccio girare la palla a seconda da dove si è venuti
+
+    if (p.palla.y == 3) { // Potrebbe esserci la paletta in corrispondenza della palla
+        if (p.matrix[p.palla.x][3] == p.skin) // Se c'è la paletta allora devi cambiare direzione come se fosse un muro
+        {
+            // Può venire da NE o da NO
+            if (p.palla.direzione == 9)
+                p.palla.direzione = 3;
+            // Se andava verso NE adesso andrà verso SE
+            if (p.palla.direzione == 7)
+                p.palla.direzione = 1;
+            // Se andava verso NO adesso andrà verso SO
+        }
     }
     else if (p.palla.x == 48)
-    {
-
-    }
-    else if (p.palla.y == 0)
-    {
-
-    }
-    else if (p.palla.y == 19)
-    {
-        
-    }
-    // Cambi la direzione a seconda da quale direzione stavi venendo
-    // Ad esempio se venivi verso Sud-Est la tua direzione cambia in Nord-Ovest
+        p.palla.x--; // NO! NON CAMBI LA POSIZIONE MA SOLO LA DIREZIONE!
+    else if (p.palla.x == 1)
+        p.palla.y--;
+    else if (p.palla.y == 18)
+        .palla.y++;
 }
+
 
 
 void aggiornaMatrice(Partita p) // Chiara
@@ -120,13 +130,22 @@ void aggiornaMatrice(Partita p) // Chiara
         for (int j=0; j<50; j++)
         {
             if(p.matrix[i][j] == 'O')
-                p.matrix[i][j] == ' ';
+                p.matrix[i][j] == ' '; // Fatto ✔️
         }
     }
     
-    // Posiziona la nuova pallina sulla base delle coordinate (p.palla.x e p.palla.y)
+    // Posiziona la nuova pallina ('O') sulla base delle coordinate (p.palla.x e p.palla.y)
+    p.matrix[p.palla.x][p.palla.y] = 'O';
 
-    // Stessa cosa per la paletta del giocatore
+    // Stessa cosa per la paletta del giocatore, prima pulisci tutta la riga 3, e poi stampi sopra le righette
+    // Consiglio: usare p.skin invece di '_' così in caso poi le cambiamo
+    for (int j=0; j<50; j++) // Guardi tutta la riga 3
+    {
+        if(p.matrix[3][j] == '_')
+            p.matrix[3][j] == ' ';
+    }
+    p.matrix[p.x][p.y] = '_';
+    
 }
 
 
